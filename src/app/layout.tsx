ad008f6836c1fc024631f73d511779d2e2eb95a1
Jorge Perez <information.jperez@gmail.com>
2024-12-03 13:17:51 +0800

    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,6 +36,7 @@ export default function RootLayout({
       >
         <Header />
           {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
